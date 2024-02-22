3105af850b2ef758824cf6a7fc58139e06ae5929
Dallas <dallas2k@163.com>
2024-02-22 16:53:39 +0800

    有问题

---
+++ BuildingIdentify/OperateExcel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NPOI.HPSF;
+using NPOI.HSSF.UserModel;
+using NPOI.SS.UserModel;
+
+namespace BuildingIdentify
+{
+    internal class OperateExcel
+    {
+    }
+
+
+
+}
--- BuildingIdentify/Program.cs
+++ BuildingIdentify/Program.cs
@@ -1,2 +1,46 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using NPOI.HSSF.UserModel;
+using NPOI.SS.UserModel;
+
+FileStream file;
+HSSFWorkbook wb;
+
+var buildingaddresspath = @"txt\楼宇地址.txt";
+var filepath = @"..\企业注册数据导出.xlsx";
+
+Dictionary<string, string> buildingAddress = ReadBuildingAddress(buildingaddresspath);
+
+Dictionary<string, string> ReadBuildingAddress(string path)
+{
+    Dictionary<string, string> result = new Dictionary<string, string>();
+
+    StreamReader sr = new StreamReader(path);
+    string? line = sr.ReadLine();
+    while (line != null)
+    {
+        string[] item = line.Split('，');
+        result.Add(item[1].Trim(), item[0].Trim());
+        line = sr.ReadLine();
+    }
+    sr.Close();
+
+    return result;
+
+}
+
+file = new FileStream(filepath, FileMode.Open, FileAccess.Read); 
+wb = new HSSFWorkbook(file);
+file.Close();
+
+ISheet sheet= wb.GetSheetAt(0);
+
+for (int i = 1; i < sheet.LastRowNum; i++)
+{
+    Console.WriteLine(sheet.GetRow(i).GetCell(10));
+}
+
+
+//foreach (var ba in buildingAddress)
+//Console.WriteLine(ba);
+
